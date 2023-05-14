bc6cbaf72128bf8e681e78aeed76c2765b1029c3
Azad-mosarof <misarofazad@gmail.com>
2023-05-14 22:47:59 +0530

    package.json update

--- src/Firebase.js
+++ src/Firebase.js
@@ -4,13 +4,13 @@ import 'firebase/compat/auth';
 import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: "FIREBASE API KEY",
-    authDomain: "AUTH-DOMAIN",
-    projectId: "PROJECT-ID",
+    apiKey: "AIzaSyBqGUwdjuiZAXE_gY3tnSGOL7eBGg_moGM",
+    authDomain: "e-clone-ee3fd.firebaseapp.com",
+    projectId: "e-clone-ee3fd",
     storageBucket: "e-clone-ee3fd.appspot.com",
-    messagingSenderId: "MESSAGING-SENDER-ID",
-    appId: "APP-ID",
-    measurementId: "MESUREMENT-ID"
+    messagingSenderId: "1022295368049",
+    appId: "1:1022295368049:web:abf39963c3ff7d903956d0",
+    measurementId: "G-34HKXDXD79"
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
